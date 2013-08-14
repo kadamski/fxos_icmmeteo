2a4fee53bf4e29e5b664952156d0ddc9b672cd01
Krzysztof Adamski <k@japko.eu>
2013-08-14 21:47:22 +0200

    Show image inside iframe and not start external browser.

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+
 function main() {
     var xmlhttp=new XMLHttpRequest({mozSystem: true});
     xmlhttp.open("GET",'http://www.meteo.pl/meteorogram_um_js.php',true);
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.status === 200 && xmlhttp.readyState === 4) {
             var date=/UM_FULLDATE="([0-9]{10})"/.exec(xmlhttp.response)[1];
-            var img=document.getElementById('img');
             var row=436;
             var col=181;
-            img.src='http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&fdate='+date+'&row='+row+'&col='+col+'&lang=pl';
-            var a = new MozActivity({ name: "view", data: { type: "url", url: img.src }});
-            a.onerror = function() { alert("Can't view the image!"); };
+            var iframe=document.createElement("IFRAME"); 
+            iframe.style.border='none';
+            function _orientChange() {
+                iframe.style.width=window.screen.width+'px';
+                iframe.style.height=window.screen.height+'px';
+            };
+            screen.addEventListener("mozorientationchange", _orientChange);
+            _orientChange();
+            iframe.src='http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&fdate='+date+'&row='+row+'&col='+col+'&lang=pl';
+            document.body.appendChild(iframe);
         }
     };
     xmlhttp.onerror = function () { alert("Can't download"); };
