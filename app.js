053db0e9136b7d558a74f8b81cea96526f848cfb
Krzysztof Adamski <k@japko.eu>
2013-08-16 12:30:54 +0200

    handling more locations, step 1.

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var UPDATE_INTERVAL=(1000*60*60);   // 1h
+var cities={
+    'Białystok': [379, 285],
+    'Wrocław': [436, 181],
+}
 
 function createFrame() {
     var frame=document.getElementById('frame');
@@ -7,6 +11,8 @@ function createFrame() {
         frame.setAttribute('id', 'frame');
         frame.setAttribute('lastDate', '0');
         frame.setAttribute('lastUpdate', '0');
+        frame.setAttribute('lastRow', '-1');
+        frame.setAttribute('lastCol', '-1');
         frame.style.border='none';
         function _orientChange() {
             frame.style.width=window.screen.width+'px';
@@ -31,14 +37,12 @@ function loadImage(frame, date, row, col) {
     frame.src='http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&fdate='+date+'&row='+row+'&col='+col+'&lang=pl';
 }
 
-function handleVisibilityChange() {
-    if(document.hidden) {
-        return;
-    }
-
+function showImage() {
     var frame=createFrame();
-
     var lastUpdate=frame.getAttribute('lastUpdate');
+    var row=frame.getAttribute('lastRow');
+    var col=frame.getAttribute('lastCol');
+
     var time = (new Date()).getTime();
     if((time-lastUpdate)<UPDATE_INTERVAL) {
         console.log("To soon!");
@@ -50,8 +54,6 @@ function handleVisibilityChange() {
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.status === 200 && xmlhttp.readyState === 4) {
             var date=/UM_FULLDATE="([0-9]{10})"/.exec(xmlhttp.response)[1];
-            var row=436;
-            var col=181;
             loadImage(frame, date, row, col);
             frame.setAttribute('lastUpdate', time);
         }
@@ -60,11 +62,34 @@ function handleVisibilityChange() {
     xmlhttp.send();
 }
 
-function main() {
-    handleVisibilityChange();
+function setPosition(row, col) {
+    var frame=createFrame();
+    var lastRow=frame.getAttribute('lastRow');
+    var lastCol=frame.getAttribute('lastCol');
+    if(lastRow==row && lastCol==col) {
+        console.log('Neighter row nor col changed');
+        return;
+    }
+    frame.setAttribute('lastUpdate', 0);
+    frame.setAttribute('lastDate', 0);
+    lastRow=frame.setAttribute('lastRow', row);
+    lastCol=frame.setAttribute('lastCol', col);
+    showImage();
 }
+
+function setCity(city) {
+    setPosition.apply(null, cities[city]);
+}
+
+function handleVisibilityChange() {
+    if(document.hidden) {
+        return;
+    }
+    showImage();
+}
+
 
 document.addEventListener("DOMContentLoaded", function () {
     document.addEventListener("visibilitychange", handleVisibilityChange);
-    main();
+    setCity('Wrocław');
 })
